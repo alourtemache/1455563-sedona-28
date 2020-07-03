f799ab5e7800e826bec3d1f749be6d8335a355b5
Alina <64503919+alourtemache@users.noreply.github.com>
2020-07-03 20:35:19 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,9 @@
 var link = document.querySelector(".search-wrapper");
 var popup = document.querySelector(".search-hotel");
-var close = document.querySelector(".search-wrapper");
-/*
-link.onclick = function() {
-    popup.classList.toggle()
-}
-*/
+
+
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
-    popup.classList.add("modal-show");
+    popup.classList.toggle("modal-show");
 });
 
-close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-show");
-});
